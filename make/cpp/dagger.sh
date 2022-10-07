8723554838be2ebb31de526c8fd4d5f381caaf92
kpenfound <kyle@dagger.io>
2022-10-07 16:58:45 -0400

    make demos: move jq inline
    Signed-off-by: kpenfound <kyle@dagger.io>

--- make/cpp/dagger.sh
+++ make/cpp/dagger.sh
@@ -4,16 +4,13 @@ touch cloak.yaml
 RUN="dagger do -f ./queries.graphql"
 
 echo "Loading workdir"
-WORK=$($RUN Workdir)
-WORKFS=$(echo -n $WORK | jq -r '.host.workdir.read.id')
+WORKFS=$($RUN Workdir | jq -r '.host.workdir.read.id')
 
 echo "Preparing Builder"
-BUILDER=$($RUN Builder)
-BUILDERID=$(echo -n $BUILDER | jq -r '.core.image.exec.fs.exec.fs.id')
+BUILDER=$($RUN Builder | jq -r '.core.image.exec.fs.exec.fs.id')
 
 echo "Doing postgresql build"
-BUILD=$($RUN Build --set "workfs=$WORKFS" --set "builderid=$BUILDERID")
-BUILDFS=$(echo -n $BUILD | jq -r '.core.filesystem.exec.mount.id')
+BUILDFS=$($RUN Build --set "workfs=$WORKFS" --set "builderid=$BUILDER" | jq -r '.core.filesystem.exec.mount.id')
 
 echo "Copying build outputs"
 CP=$($RUN Copy --local-dir bin=./bin --set "buildfs=$BUILDFS")
--- make/hello/dagger.sh
+++ make/hello/dagger.sh
@@ -3,13 +3,11 @@ touch cloak.yaml
 
 RUN="dagger do -f ./queries.graphql"
 
-WORK=$($RUN Workdir)
-WORKFS=$(echo -n $WORK | jq -r '.host.workdir.read.id')
+WORKFS=$($RUN Workdir | jq -r '.host.workdir.read.id')
 
 TASK="hello"
-HELLO=$($RUN --set "workfs=$WORKFS" --set "task=$TASK" Make)
-OUT=$(echo -n $HELLO | jq -r '.core.image.exec.fs.exec.stdout')
-echo $OUT
+HELLO=$($RUN --set "workfs=$WORKFS" --set "task=$TASK" Make | jq -r '.core.image.exec.fs.exec.stdout')
+echo $HELLO
 
 #cleanup
 rm cloak.yaml
