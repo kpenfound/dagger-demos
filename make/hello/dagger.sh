5b34a5f5918dd15dd68123d5beb171ca074fdd74
kpenfound <kyle@dagger.io>
2022-10-07 10:50:55 -0400

    move heredoc queries to gql file
    Signed-off-by: kpenfound <kyle@dagger.io>

--- make/hello/dagger.sh
+++ make/hello/dagger.sh
@@ -1,47 +1,13 @@
 #!/bin/bash
 touch cloak.yaml
 
-WORK=$(dagger do<<'EOF'
-query {
-    host {
-        workdir {
-            read {
-                id
-            }
-        }
-    }
-}
-EOF
-)
+RUN="dagger do -f ./queries.graphql"
+
+WORK=$($RUN Workdir)
 WORKFS=$(echo -n $WORK | jq -r '.host.workdir.read.id')
 
-HELLO=$(dagger --set "workfs=$WORKFS" do<<'EOF'
-query ($workfs: FSID!) {
-  core {
-    image(ref: "alpine") {
-      exec(input: {
-        args: ["apk", "add", "make"]
-      }) {
-        fs {
-            exec(input: {
-            args: ["make", "hello"]
-            workdir: "/src"
-            mounts: [
-                {
-                    path: "/src"
-                    fs: $workfs
-                }
-            ]
-        }) {
-            stdout
-        }
-        }
-      }
-    }
-  }
-}
-EOF
-)
+TASK="hello"
+HELLO=$($RUN --set "workfs=$WORKFS" --set "task=$TASK" Make)
 OUT=$(echo -n $HELLO | jq -r '.core.image.exec.fs.exec.stdout')
 echo $OUT
 
