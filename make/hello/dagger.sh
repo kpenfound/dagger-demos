5eeb368d5f09865aa515307417b1530bc9a7849e
kpenfound <kyle@dagger.io>
2022-10-12 14:59:06 -0400

    update api
    Signed-off-by: kpenfound <kyle@dagger.io>

--- make/hello/dagger.sh
+++ make/hello/dagger.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-touch cloak.yaml
+echo "name: hello" >> cloak.yaml
 
 RUN="dagger do -f ./queries.graphql"
 
 WORKFS=$($RUN Workdir | jq -r '.host.workdir.read.id')
 
 TASK="hello"
-HELLO=$($RUN --set "workfs=$WORKFS" --set "task=$TASK" Make | jq -r '.core.image.exec.fs.exec.stdout')
+HELLO=$($RUN --set "workfs=$WORKFS" --set "task=$TASK" Make | jq -r '.container.from.exec.withMountedDirectory.withWorkdir.exec.stdout.contents')
 echo $HELLO
 
 #cleanup
