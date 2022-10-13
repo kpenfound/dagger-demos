c3fbe4dca538d835c0a737933925b4a4cc9208db
kpenfound <kyle@dagger.io>
2022-10-13 10:23:43 -0400

    wip
    Signed-off-by: kpenfound <kyle@dagger.io>

--- go/hello-container/magefiles/main.go
+++ go/hello-container/magefiles/main.go
@@ -10,12 +10,18 @@ import (
 	"go.dagger.io/dagger/sdk/go/dagger/api"
 )
 
+const (
+	golangImage    = "golang:latest"
+	baseImage      = "alpine:latest"
+	publishAddress = "ghcr.io/kpenfound/hello-container:latest"
+)
+
 func Run(ctx context.Context) {
 	if err := engine.Start(ctx, &engine.Config{}, func(ctx engine.Context) error {
 		core := api.New(ctx.Client)
 
 		// Load image
-		golang := core.Container().From("golang:latest")
+		golang := core.Container().From(golangImage)
 
 		// Set workdir
 		src, err := core.Host().Workdir().Read().ID(ctx)
@@ -48,7 +54,7 @@ func Test(ctx context.Context) {
 		core := api.New(ctx.Client)
 
 		// Load image
-		golang := core.Container().From("golang:latest")
+		golang := core.Container().From(golangImage)
 
 		// Set workdir
 		src, err := core.Host().Workdir().Read().ID(ctx)
@@ -69,6 +75,51 @@ func Test(ctx context.Context) {
 		}
 
 		fmt.Println(out)
+
+		return nil
+	}); err != nil {
+		panic(err)
+	}
+}
+
+func Push(ctx context.Context) {
+	if err := engine.Start(ctx, &engine.Config{}, func(ctx engine.Context) error {
+		core := api.New(ctx.Client)
+
+		// Load image
+		builder := core.Container().From(golangImage)
+
+		// Set workdir
+		src, err := core.Host().Workdir().Read().ID(ctx)
+		if err != nil {
+			return err
+		}
+		builder = builder.WithMountedDirectory("/src", src).WithWorkdir("/src")
+
+		// Execute Command
+		builder = builder.Exec(api.ContainerExecOpts{
+			Args: []string{"go", "build", "-o", "hello"},
+		})
+
+		// Get built binary
+		helloBin, err := builder.File("/src/hello").ID(ctx)
+		if err != nil {
+			return err
+		}
+
+		// Get base image for publishing
+		base := core.Container().From(baseImage)
+		// Add built binary to /bin
+		base = base.WithMountedFile("/bin/hello", helloBin)
+		// Set entrypoint
+		base = base.WithEntrypoint([]string{"/bin/hello"})
+		// Publish image
+		addr, err := base.Publish(ctx, publishAddress)
+		if err != nil {
+			return err
+		}
+
+		fmt.Println(addr)
 
 		return nil
 	}); err != nil {
